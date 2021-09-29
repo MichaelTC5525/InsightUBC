9bdaddaafc6b1d9da547e46e43446f4b90ab62f1
Michael Cheung <michael.cheung@live.ca>
2021-09-29 02:04:35 -0700

    Add trivial non-query method test cases

--- test/controller/InsightFacade.spec.ts
+++ test/controller/InsightFacade.spec.ts
@@ -16,6 +16,13 @@ describe("InsightFacade", function () {
 	// automatically be loaded in the 'before' hook.
 	const datasetsToLoad: {[key: string]: string} = {
 		courses: "./test/resources/archives/courses.zip",
+		courses1: "./test/resources/archives/validSet.zip",
+		courses2: "./test/resources/archives/set5000.zip",
+		emptySet: "./test/resources/archives/emptySet.zip",
+		ignoreFolderSet: "./test/resources/archives/ignoreFolderSet.zip",
+		invalidSetDir: "./test/resources/archives/invalidSetDir.zip",
+		invalidSetJSON: "./test/resources/archives/invalidSetJSON.zip",
+		skipOverSet: "./test/resources/archives/skipOverSet.zip"
 	};
 
 	before(function () {
@@ -51,13 +58,116 @@ describe("InsightFacade", function () {
 			fs.removeSync(persistDir);
 		});
 
-		// This is a unit test. You should create more like this!
-		it("Should add a valid dataset", function () {
-			const id: string = "courses";
-			const content: string = datasetContents.get("courses") ?? "";
-			const expected: string[] = [id];
-			return insightFacade.addDataset(id, content, InsightDatasetKind.Courses).then((result: string[]) => {
-				expect(result).to.deep.equal(expected);
+		describe("InsightFacade.addDataset Test Cases", function () {
+			context("Success: Add a dataset to a new database", () => {
+				it("Should have a single dataset ID in the database", () => {
+					const id: string = "courses1";
+					const content: string = datasetContents.get("courses1") ?? "";
+					const expected: string[] = [id];
+					return insightFacade.addDataset(id, content, InsightDatasetKind.Courses)
+						.then((result: string[]) => {
+							expect(result).to.deep.equal(expected);
+						});
+				});
+			});
+
+			context("Success: Add a dataset to a database with one existing dataset", () => {
+				it("Should have 2 dataset IDs in the database", async () => {
+					const content: string = datasetContents.get("courses1") ?? "";
+					const expected: string[] = ["1", "2"];
+					// Add one dataset successfully beforehand
+					await insightFacade.addDataset("1", content, InsightDatasetKind.Courses);
+					return insightFacade.addDataset("2", content, InsightDatasetKind.Courses)
+						.then((result: string[]) => {
+							expect(result).to.deep.equal(expected);
+						});
+				});
+			});
+
+			context("Success: Add a dataset to a database with multiple existing datasets", () => {
+				it("Should have more than 2 dataset IDs in the database", async () => {
+					const content: string = datasetContents.get("courses1") ?? "";
+					const expected: string[] = ["1", "2", "3"];
+					// Add two datasets successfully beforehand
+					await insightFacade.addDataset("1", content, InsightDatasetKind.Courses);
+					await insightFacade.addDataset("2", content, InsightDatasetKind.Courses);
+					return insightFacade.addDataset("3", content, InsightDatasetKind.Courses)
+						.then((result: string[]) => {
+							expect(result).to.deep.equal(expected);
+						});
+				});
+			});
+
+			context("Success: Add a dataset while skipping over an invalid file", () => {
+				it("Should add a dataset with no error, with results from only valid files", () => {
+					const content: string = datasetContents.get("skipOverSet") ?? "";
+					const expected: string[] = ["skipOverSet"];
+					return insightFacade.addDataset("skipOverSet", content, InsightDatasetKind.Courses)
+						.then((result: string[]) => {
+							expect(result).to.deep.equal(expected);
+						});
+				});
+			});
+
+			context("Success: Add a dataset while ignoring folders other than 'courses' in ZIP", () => {
+				it("Should add a dataset with no error, ignoring other folders in the ZIP", () => {
+					const content: string = datasetContents.get("ignoreFolderSet") ?? "";
+					const expected: string[] = ["ignoreFolderSet"];
+					return insightFacade.addDataset("ignoreFolderSet", content, InsightDatasetKind.Courses)
+						.then((result: string[]) => {
+							expect(result).to.deep.equal(expected);
+						});
+				});
+			});
+		});
+
+		describe("InsightFacade.removeDataset Test Cases", function () {
+			context("Success: Remove a dataset by matching ID", () => {
+				it("Should take out a specified dataset by id value", async () => {
+					const id: string = "courses1";
+					const content: string = datasetContents.get("courses1") ?? "";
+					const expected: string = id;
+					await insightFacade.addDataset(id, content, InsightDatasetKind.Courses);
+					return insightFacade.removeDataset(id)
+						.then((result: string) => {
+							expect(result).to.deep.equal(id);
+						});
+				});
+			});
+		});
+
+		describe("InsightFacade.listDataset Test Cases", function () {
+			context("Success: Retrieve the list of datasets when empty", () => {
+				it("Should return an empty list as no datasets are present or added", () => {
+					return insightFacade.listDatasets()
+						.then((result: any[]) => {
+							expect(result).to.have.length(0);
+							expect(result).to.deep.equal([]);
+						});
+				});
+			});
+
+			context("Success: Retrieve the list of datasets with only one dataset added", () => {
+				it("Should return a list containing a single entry corresponding to the one dataset", async () => {
+					const content: string = datasetContents.get("courses1") ?? "";
+					await insightFacade.addDataset("1", content, InsightDatasetKind.Courses);
+					return insightFacade.listDatasets()
+						.then((result: any[]) => {
+							expect(result).to.have.length(1);
+						});
+				});
+			});
+
+			context("Success: Retrieve the list of datasets with multiple datasets added", () => {
+				it("Should return a list containing more than a single entry", async () => {
+					const content: string = datasetContents.get("courses1") ?? "";
+					await insightFacade.addDataset("1", content, InsightDatasetKind.Courses);
+					await insightFacade.addDataset("2", content, InsightDatasetKind.Courses);
+					return insightFacade.listDatasets()
+						.then((result: any[]) => {
+							expect(result).to.have.length(2);
+						});
+				});
 			});
 		});
 	});
