10ef056b4f4a22bdd93943ea2952d2e81c5ac60d
Michael Cheung <michael.cheung@live.ca>
2021-10-02 02:28:06 -0700

    Clean up lint issues

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,6 +1,5 @@
 import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, NotFoundError} from "./IInsightFacade";
-import * as fs from 'fs-extra';
-import 'jszip';
+import * as fs from "fs-extra";
 import JSZip from "jszip";
 
 /**
@@ -10,14 +9,16 @@ import JSZip from "jszip";
  */
 export default class InsightFacade implements IInsightFacade {
 
-	datasetStorage: InsightDataset[];
-	courseSections: Map<string, string>;
+	private datasetStorage: InsightDataset[];
+
+	// Will likely make a custom class for this as [] type
+	private courseSections: Map<string, string>;
 
 	/**
 	 * On creating a new InsightFacade instance, we should read the ./data file system directory
 	 * in order to ensure that we reload any existing datasets and course sections that were left from
 	 * a previous instance of the class
-	 */ 
+	 */
 	constructor() {
 		console.trace("InsightFacadeImpl::init()");
 		this.datasetStorage = [];
@@ -29,7 +30,7 @@ export default class InsightFacade implements IInsightFacade {
 		let currSets: string[] = [];
 
 		// Grab the dataset ids that are already in the database
-		for (var dataset of this.datasetStorage) {
+		for (let dataset of this.datasetStorage) {
 			currSets.push(dataset.id);
 		}
 
@@ -47,25 +48,24 @@ export default class InsightFacade implements IInsightFacade {
 		// Read in the .ZIP file
 		let zip = new JSZip();
 		zip.loadAsync(content, {base64: true})
-			.then((zip) => {
+			.then((result) => {
 				// Search through and parse the valid JSON files
-				
 			})
 			.catch((error) => {
-				// Remove the folder in case it was created asynchronously
+				// Remove the folder if created
 				zip.remove("./data");
-				return Promise.reject("Something went wrong loading the dataset. Ensure your .zip is in base64 string format");
+				return Promise.reject("Dataset could not be loaded. Ensure .zip is in base64 string format");
 			});
 
 		// Create a data folder to use as the space on disk
-		zip.folder("./data/" + id + "/")
+		zip.folder("./data/" + id + "/");
 
 		// Create an InsightDataset object to track high-level information of the dataset being added
 		let newDataset: InsightDataset = {
 			id: id,
 			kind: kind,
 			numRows: 0   // to be variable
-		}
+		};
 		currSets.push(id);
 		this.datasetStorage.push(newDataset);
 		return Promise.resolve(currSets);
