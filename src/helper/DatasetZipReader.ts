7d1661092a6edd6f3248ab19585c9ba48aabb9cc
Michael Cheung <michael.cheung@live.ca>
2021-10-30 21:56:01 -0700

    Build algorithm to extract file links to ZIP locations, http request

--- src/helper/DatasetZipReader.ts
+++ src/helper/DatasetZipReader.ts
@@ -4,7 +4,7 @@ import CourseSection from "../storageType/CourseSection";
 import {DatasetEntry} from "../storageType/DatasetEntry";
 import Room from "../storageType/Room";
 import * as p5 from "parse5";
-import {Document} from "parse5";
+import {ChildNode, Document} from "parse5";
 
 const http = require("http");
 
@@ -27,7 +27,7 @@ export default class DatasetZipReader {
 			});
 
 			return Promise.all(promises).then((files) => {
-				totalResult = this.handleCourseFiles(folder, files);
+				totalResult = this.handleCourseFiles(files);
 				return Promise.resolve(totalResult);
 			});
 		} else if (folder === "rooms") {
@@ -35,41 +35,104 @@ export default class DatasetZipReader {
 			if (index === null) {
 				throw new InsightError("Zip file is missing index.htm in root directory under 'rooms'");
 			}
-			index.async("string").then((indexContent) => {
+			return index.async("string").then((indexContent) => {
 				return p5.parse(indexContent);
 			}).then((document) => {
-				// TODO: Find <a href>s for links to other files in ZIP
-				let filesToCheck: string[] = this.findRoomBuildingFiles(document);
-				return this.handleRoomFiles(folder, contentZip, filesToCheck);
-			}).then((entries) => {
-				// TODO
+				let indexInfo = this.findIndexBuildingInfo(document);
+				return this.handleRoomFiles(folder, contentZip, indexInfo);
+			}).then((entries: DatasetEntry[]) => {
+				return Promise.resolve(entries);
 			});
-
-			return Promise.resolve(totalResult);
 		} else {
 			throw new InsightError("Unreachable: folder to check for should be either 'courses' or 'rooms'");
 		}
 	}
 
-	private findRoomBuildingFiles(document: Document): string[] {
-		// TODO: extract list of href from "a" elements in corresponding table
+	private findIndexBuildingInfo(document: any): string[][] {
 		let filePaths: string[] = [];
-		// Base case: should occur when within an "a" tag
-		let currElem = document["childNodes"];
-		// Document -> html -> body -> div class="view-content" -> table class -> tbody
-		for (let c of document.childNodes) {
-			if (c.nodeName === "html") {
-				//
-			}
-		}
-		return filePaths;
+		let buildingNames: string[] = [];
+		let buildingCodes: string[] = [];
+		let buildingAddresses: string[] = [];
+		this.extractBuildingInfoInto(filePaths, buildingNames, buildingCodes, buildingAddresses, document);
+		return [filePaths, buildingNames, buildingCodes, buildingAddresses];
 	}
 
-	private handleRoomFiles(folder: string, contentZip: JSZip, files: string[]): Promise<DatasetEntry[]> {
+	private extractBuildingInfoInto(filePaths: string[], buildingNames: string[], buildingCodes: string[],
+		buildingAddresses: string[], node: any) {
+		// Setup base case ; sanity check that a link is only pushed if it is intending to go somewhere within ZIP
+		if (node.nodeName === "a") {
+			this.extractFromLink(node, filePaths, buildingNames);
+		} else if (node.nodeName === "td") {
+			if (node.attrs) {
+				for (let attr of node.attrs) {
+					if (attr.name === "class") {
+						if (attr.value === "views-field views-field-field-building-code") {
+							for (let n of node.childNodes) {
+								if (n.nodeName === "#text") {
+									// No duplicate codes
+									if (!buildingCodes.includes(n.value.replace("\n", "").trim())) {
+										buildingCodes.push(n.value.replace("\n", "").trim());
+									}
+								}
+							}
+						}
+						if (attr.value === "views-field views-field-field-building-address") {
+							for (let n of node.childNodes) {
+								// Allow for locations to have the same address when not distinguished by units or other
+								if (n.nodeName === "#text") {
+									buildingAddresses.push(n.value.replace("\n", "").trim());
+								}
+							}
+						}
+						if (attr.value === "views-field views-field-title") {
+							for (let childNode of node.childNodes) {
+								this.extractBuildingInfoInto(filePaths, buildingNames, buildingCodes,
+									buildingAddresses, childNode);
+							}
+						}
+					}
+				}
+			}
+		} else {
+			if (node.childNodes) {
+				for (let childNode of node.childNodes) {
+					this.extractBuildingInfoInto(filePaths, buildingNames, buildingCodes, buildingAddresses, childNode);
+				}
+			}
+		}
+	}
+
+	private extractFromLink(node: any, filePaths: string[], buildingNames: string[]) {
+		if (node.attrs && node.attrs.length > 1) {
+			for (let attr of node.attrs) {
+				if (attr.name === "href") {
+					if (attr.value.substr(0, 2) === "./") {
+						if (!filePaths.includes(attr.value)) {
+							filePaths.push(attr.value);
+						}
+					}
+				}
+				if (attr.name === "title") {
+					if (attr.value === "Building Details and Map") {
+						for (let n of node.childNodes) {
+							if (n.nodeName === "#text") {
+								// No duplicate names
+								if (!buildingNames.includes(n.value)) {
+									buildingNames.push(n.value);
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	private handleRoomFiles(folder: string, contentZip: JSZip, indexInfo: string[][]): Promise<DatasetEntry[]> {
 		let promises: Array<Promise<string>> = [];
-		for (let f of files) {
+		for (let file of indexInfo[0]) {
 			// <a href = "./campus/directories/ ... " ; remove prefix ./
-			let filePath: string = f.split("./")[1];
+			let filePath: string = file.split("./")[1];
 			const currFile: JSZip.JSZipObject | null = contentZip.file(folder + "/" + filePath);
 			// If file that is in the link non-existent in dataset, skip
 			if (currFile === null) {
@@ -79,26 +142,60 @@ export default class DatasetZipReader {
 		}
 
 		return Promise.all(promises).then((filesContent) => {
-			let results: DatasetEntry[] = [];
-			for (let fc of filesContent) {
-				let doc: Document = p5.parse(fc);
-				results = results.concat(this.findRoomRows(doc));
+			// indexInfo has fullname, shortname, address
+			// filesContent has number, seats, type, furniture
+			// lat, lon from http request
+			// href from specific file <a href="https://students.ubc.ca/...
+			let retEntries: DatasetEntry[] = [];
+
+			// TODO: extract number, seats, type, furniture from a linked file
+			let roomDets: Array<[string, number, string, string]>;
+			for (let f of filesContent) {
+				// Parse, gives number, seats, type, furniture
+				// TODO: make room entries
+				let doc: Document = p5.parse(f);
+				retEntries.concat(this.findRoomRows(doc, null));
 			}
-			return Promise.resolve(results);
+			let index: number = 0;
+			// indexInfo[0] is filePaths, indexInfo[1] is building names, indexInfo[2] is building codes, indexInfo[3] is addresses
+			// retEntries.push(new Room(indexInfo[1][index], indexInfo[2][index], number, indexInfo[3][index]));
+			return Promise.resolve(retEntries);
 		});
 	}
 
-	private findRoomRows(document: Document): DatasetEntry[] {
+	private findRoomRows(document: Document, childNode: ChildNode | null): DatasetEntry[] {
+		let obj: any = {};
+		let buildingAddress: string = "6245%20Agronomy%20Road%20V6T%201Z4";
+		buildingAddress.replace(" ", "%20");
+
+		// Credits to https://nodejs.org/en/knowledge/HTTP/clients/how-to-create-a-HTTP-request/
+		const options = {
+			host: "http://cs310.students.cs.ubc.ca",
+			path: "/api/v1/project_team115/" + buildingAddress,
+			port: 11316,
+			method: "GET"
+		};
+
+		let callback = function (response: any) {
+			response.on("end", (data: any) => {
+				if (response.statusCode !== 200) {
+					return;
+				}
+				let geoResponse = JSON.parse(data);
+				obj.lat = geoResponse.lat;
+				obj.lon = geoResponse.lon;
+			});
+		};
+
+		return http.request(options, callback).then();
+
 		let rows: DatasetEntry[] = [];
 		// TODO: find the table in there
-		let buildingAddress: string = "";
-		let httpAddress: string = "http://cs310.students.cs.ubc.ca:11316/api/v1/project_team115/" + buildingAddress;
-		// TODO: HTTP GET REQUEST
-		const request = http.get(httpAddress);
+
 		return rows;
 	}
 
-	private handleCourseFiles(folder: string, files: string[]) {
+	private handleCourseFiles(files: string[]) {
 		let courses: DatasetEntry[] = [];
 		for (let f of files) {
 			try {
@@ -107,7 +204,7 @@ export default class DatasetZipReader {
 					// Skip this file, either empty or no entries correctly listed
 					continue;
 				}
-				let singleFileResult: DatasetEntry[] = this.addValidRows(folder, obj);
+				let singleFileResult: DatasetEntry[] = this.addValidCourses(obj);
 				for (let result of singleFileResult) {
 					courses.push(result);
 				}
@@ -121,32 +218,21 @@ export default class DatasetZipReader {
 		return courses;
 	}
 
-	private addValidRows(folder: string, obj: any): DatasetEntry[] {
+	private addValidCourses(obj: any): DatasetEntry[] {
 		let result: DatasetEntry[] = [];
-		if (folder === "courses") {
-			for (let r of obj.result) {
-				if (r.Subject === undefined || r.Course === undefined || r.Avg === undefined ||
-					r.Professor === undefined || r.Title === undefined || r.Pass === undefined ||
-					r.Fail === undefined || r.Audit === undefined || r.id === undefined ||
-					r.Year === undefined) {
-					continue;
-				}
+		for (let r of obj.result) {
+			if (r.Subject === undefined || r.Course === undefined || r.Avg === undefined ||
+				r.Professor === undefined || r.Title === undefined || r.Pass === undefined ||
+				r.Fail === undefined || r.Audit === undefined || r.id === undefined ||
+				r.Year === undefined) {
+				continue;
+			}
 
-				if (r.Section === "overall") {
-					r.Year = 1900;
-				}
-				result.push(new CourseSection(r.Subject, r.Course, r.Avg, r.Professor, r.Title,
-					r.Pass, r.Fail, r.Audit, r.id, r.Year));
+			if (r.Section === "overall") {
+				r.Year = 1900;
 			}
-		} else if (folder === "rooms") {
-			// TODO: Behaviour not yet defined; placeholder code
-			for (let r of obj.result) {
-				if (r.RoomNumber === undefined || r.Building === undefined || r.Capacity === undefined) {
-					continue;
-				}
-				result.push(new Room(r.fullname, r.shortname, r.number, r.address, r.lat, r.lon, r.seats, r.type,
-					r.furniture, r.href));
-			}
+			result.push(new CourseSection(r.Subject, r.Course, r.Avg, r.Professor, r.Title,
+				r.Pass, r.Fail, r.Audit, r.id, r.Year));
 		}
 		return result;
 	}
