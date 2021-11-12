d2f88d1c17a681a010dcb4258a95be4293937693
Michael Cheung <michael.cheung@live.ca>
2021-11-11 17:41:26 -0800

    Create minimal HTML layout; add event listeners to buttons

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,5 +1,30 @@
-document.getElementById("click-me-button").addEventListener("click", handleClickMe);
+document.getElementById("addDatasetButton").addEventListener("click", handleAdd);
+document.getElementById("removeDatasetButton").addEventListener("click", handleRemove);
+document.getElementById("performQueryButton").addEventListener("click", handleQuery);
+// document.getElementById("listDatasetsButton").addEventListener("click", handleList);
 
-function handleClickMe() {
-	alert("Button Clicked!");
+function handleAdd() {
+	alert("TODO: Add a dataset");
 }
+
+function handleRemove() {
+	alert("TODO: Remove a dataset");
+}
+
+function handleQuery() {
+	alert("TODO: Query a dataset");
+}
+
+// TODO: functionally equivalent to setting HTML link to /datasets page; safe to remove if needed
+// function handleList() {
+// 	const httpRequest = new XMLHttpRequest();
+// 	const url = "http://localhost:4321/datasets";
+// 	httpRequest.open("GET", url);
+// 	httpRequest.send();
+//
+// 	httpRequest.onreadystatechange = function() {
+// 		if (this.readyState === 4 && this.status === 200) {
+// 			console.log(httpRequest.responseText);
+// 		}
+// 	}
+// }
