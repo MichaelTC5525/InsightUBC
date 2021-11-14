37c48470bf308bc6399e2a72bf4e74f4089ae3f2
Michael Cheung <michael.cheung@live.ca>
2021-11-14 02:56:57 -0800

    (WIP) Build forms and requests for add/remove dataset

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,18 +1,61 @@
-document.getElementById("addDatasetButton").addEventListener("click", handleAdd);
-document.getElementById("removeDatasetButton").addEventListener("click", handleRemove);
+document.getElementById("addDatasetForm").addEventListener("submit", handleAdd);
+document.getElementById("removeDatasetForm").addEventListener("submit", handleRemove);
 document.getElementById("performQueryButton").addEventListener("click", handleQuery);
 // document.getElementById("listDatasetsButton").addEventListener("click", handleList);
 
-function handleAdd() {
-	alert("TODO: Add a dataset");
+async function handleAdd() {
+	let id = document.getElementById("addID").value;
+	let kind;
+	if (document.getElementById("addCourses").checked) {
+		kind = document.getElementById("addCourses").value;
+	} else {
+		kind = document.getElementById("addRooms").value;
+	}
+
+	// TODO: Extract actual raw content from ZIP file
+	let content = document.getElementById("addFile").value;
+	alert(content);
+	await fetch("http://localhost:4321/dataset/" + id + "/" + kind, {
+		method: "PUT",
+		body: content
+	}).then(
+		response => response.json()
+	).then(
+		data => console.log(data)
+	);
 }
 
-function handleRemove() {
-	alert("TODO: Remove a dataset");
+async function handleRemove() {
+	let setID = document.getElementById("toRemoveID").value;
+	let url = "http://localhost:4321/dataset/" + setID;
+	await fetch(url, {
+		method: "DELETE"
+	}).then(
+		response => response.json()
+	).then(
+		data => console.log(data)
+	)
 }
 
-function handleQuery() {
+async function handleQuery() {
+	// TODO: obtain query components from a form
 	alert("TODO: Query a dataset");
+	await fetch("http://localhost:4321/query", {
+		method: "POST",
+		body: JSON.stringify(
+			{
+				"WHERE": {
+
+				},
+				"OPTIONS": {
+					"COLUMNS":[
+
+					]
+				}
+			}
+		)
+	})
+
 }
 
 // TODO: functionally equivalent to setting HTML link to /datasets page; safe to remove if needed
