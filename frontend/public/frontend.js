98ec5afc6fce1a42c2cb6e3c274285bc5bd62102
Michael Cheung <michael.cheung@live.ca>
2021-11-16 03:19:46 -0800

    (WIP) Build interface and checks for querying

--- frontend/public/frontend.js
+++ frontend/public/frontend.js
@@ -1,8 +1,19 @@
 document.getElementById("addDatasetForm").addEventListener("submit", handleAdd);
 document.getElementById("removeDatasetForm").addEventListener("submit", handleRemove);
 document.getElementById("queryForm").addEventListener("submit", handleQuery);
-// document.getElementById("performQueryButton").addEventListener("click", handleQuery);
-// document.getElementById("listDatasetsButton").addEventListener("click", handleList);
+document.getElementById("queryCoursesType").addEventListener("change", unlockColumns);
+document.getElementById("queryRoomsType").addEventListener("change", unlockColumns);
+
+document.getElementById("yesOrder").addEventListener("change", unlockOrders);
+document.getElementById("noOrder").addEventListener("change", unlockOrders);
+
+for (let i of document.getElementsByName("columnsCourseSelect")) {
+	i.addEventListener("change", editOrderKeys);
+}
+
+for (let i of document.getElementsByName("columnsRoomSelect")) {
+	i.addEventListener("change", editOrderKeys);
+}
 
 async function handleAdd() {
 	let id = document.getElementById("addID").value;
@@ -14,7 +25,6 @@ async function handleAdd() {
 	}
 
 	// TODO: Extract actual raw content from ZIP file
-	alert("well");
 
 	const zipReader = document.getElementById("addFile").files[0].stream().getReader();
 
@@ -25,7 +35,9 @@ async function handleAdd() {
 		console.log(error);
 	});
 	alert(content);
-	await fetch("http://localhost:4321/dataset/" + id + "/" + kind, {
+	let url = "http://localhost:4321/dataset/" + id + "/" + kind;
+	alert(url);
+	await fetch(url, {
 		method: "PUT",
 		body: content
 	}).then(
@@ -38,20 +50,55 @@ async function handleAdd() {
 async function handleRemove() {
 	let setID = document.getElementById("toRemoveID").value;
 	let url = "http://localhost:4321/dataset/" + setID;
+
 	await fetch(url, {
 		method: "DELETE"
 	}).then(
 		response => response.json()
-	).then(
-		data => console.log(data)
-	)
+	).then((response) => {
+		// TODO: create confirmation messages?
+		if (response.statusCode === 200) {
+			let confirmationElement = document.createElement("p");
+			let confirmationMsg = document.createTextNode("Dataset " + setID + " successfully removed from InsightUBC");
+			confirmationElement.appendChild(confirmationMsg);
+			document.getElementById("removeDatasetForm").appendChild(confirmationElement);
+		} else {
+			let errorElement = document.createElement("p");
+			let errorMsg = document.createTextNode("Error " + response.statusCode + ": " + response.err);
+			errorElement.appendChild(errorMsg);
+			document.getElementById("removeDatasetForm").appendChild(errorElement);
+		}
+	});
+
 }
 
 async function handleQuery() {
 	// TODO: obtain query components from a form
-	alert("TODO: Query a dataset");
-	let query = {};
+	// Default form of query, must have these pieces, even if empty
+	let query = {"WHERE":{}, "OPTIONS":{"COLUMNS":[]}};
 
+	let querySet = document.getElementById("querySet").value;
+
+	let setTypeCourses = document.getElementById("queryCoursesType");
+	let setTypeRooms = document.getElementById("queryRoomsType");
+
+	let courseColumns = document.getElementsByName("columnsCourseSelect");
+	let roomColumns = document.getElementsByName("columnsRoomSelect");
+
+	// Columns lists will be of type HTMLElement[]
+	if (setTypeCourses.checked) {
+		for (let cc of courseColumns) {
+			if (cc.checked) {
+				query.OPTIONS.COLUMNS.push(querySet + cc.value);
+			}
+		}
+	} else if (setTypeRooms.checked) {
+		for (let rc of roomColumns) {
+			if (rc.checked) {
+				query.OPTIONS.COLUMNS.push(querySet + rc.value);
+			}
+		}
+	}
 
 	await fetch("http://localhost:4321/query", {
 		method: "POST",
@@ -63,16 +110,50 @@ async function handleQuery() {
 	);
 }
 
-// TODO: functionally equivalent to setting HTML link to /datasets page; safe to remove if needed
-// function handleList() {
-// 	const httpRequest = new XMLHttpRequest();
-// 	const url = "http://localhost:4321/datasets";
-// 	httpRequest.open("GET", url);
-// 	httpRequest.send();
-//
-// 	httpRequest.onreadystatechange = function() {
-// 		if (this.readyState === 4 && this.status === 200) {
-// 			console.log(httpRequest.responseText);
-// 		}
-// 	}
-// }
+function unlockColumns() {
+	if (document.getElementById("queryCoursesType").checked) {
+		for (let column of document.getElementsByName("columnsCourseSelect")) {
+			column.disabled = false;
+		}
+		for (let column of document.getElementsByName("columnsRoomSelect")) {
+			column.disabled = true;
+		}
+	} else {
+		for (let column of document.getElementsByName("columnsCourseSelect")) {
+			column.disabled = true;
+		}
+		for (let column of document.getElementsByName("columnsRoomSelect")) {
+			column.disabled = false;
+		}
+	}
+}
+
+function unlockOrders() {
+	if (document.getElementById("yesOrder").checked) {
+		for (let dir of document.getElementsByName("orderDir")) {
+			dir.disabled = false;
+		}
+		for (let key of document.getElementsByName("orderKeys")) {
+			key.disabled = false;
+		}
+	} else {
+		for (let dir of document.getElementsByName("orderDir")) {
+			dir.disabled = true;
+		}
+		for (let key of document.getElementsByName("orderKeys")) {
+			key.disabled = true;
+		}
+	}
+}
+
+function editOrderKeys() {
+	// TODO: dynamically add elements?
+	let placeToPlace = document.getElementById("dynamicOrderKeysList");
+
+	let input = document.createElement("input");
+	input.setAttribute("id", )
+	input.setAttribute("type", "checkbox");
+
+	let label = document.createElement
+	placeToPlace.appendChild(document.createElement())
+}
